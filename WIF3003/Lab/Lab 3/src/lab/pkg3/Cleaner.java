bff5cbf140a130bb1d884cdd093562f4e78d68b8
user <eisyad.irsyad@gmail.com>
2018-03-16 11:30:22 +0800

    Discussed answer in class

---
+++ WIF3003/Lab/Lab 3/src/lab/pkg3/Cleaner.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg3;
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class Cleaner extends Thread{
+
+    private Room room;
+    
+    public Cleaner(Room room_number){
+        room=room_number;
+    }
+    
+    public void run(){
+        try{
+            sleep(300);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        try{
+            room.cleanerEnter();
+        }catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        try{
+            sleep(300);
+        }catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        room.cleanerExit();
+    }
+}
--- WIF3003/Lab/Lab 3/src/lab/pkg3/Room.java
+++ WIF3003/Lab/Lab 3/src/lab/pkg3/Room.java
@@ -13,47 +13,42 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class Room{
-	private int guests,cleaners;
-	Room(){
-		guests = 0;
-		cleaners = 0;
-	}
+    int guests=0,cleaners=0;
 
-	public synchronized void guestEnter(int guestsEnter){
-            if((guests+guestsEnter)>5){
-                System.err.println("More than 5 guests are trying to enter the room !");
-            }else{
-                if(cleaners != 0){
-			try{
-				System.out.println("Guests are waiting, currently have "+cleaners+" cleaners cleaning the room !");
-				wait();
-			}catch(Exception e){}
-		}
-                    guests+=guestsEnter;
-                    System.out.println(guestsEnter+" guests entered the room."); 
-            }
-	}
+    public int getGuests() {
+        return guests;
+    }
 
-	public synchronized void cleanerEnter(int cleanersEnter){
-		if(guests != 0){
-			try{
-				System.out.println("Cleaners are waiting, currently have "+guests+" guests in the room !");
-                                wait();
-                        }catch(Exception e){}
-		}
-		cleaners+=cleanersEnter;
-		System.out.println(cleanersEnter+" cleaners entered the room.");
-	}
+    public int getCleaners() {
+        return cleaners;
+    }
+        
+    public Room(){
+    }
 
-	public synchronized void allLeft(){
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            System.out.println(cleaners+" cleaners and "+guests+" guests left !");
-            cleaners = 0;
-            guests = 0;
-            notifyAll();
-	}
+    public synchronized void guestEnter() throws InterruptedException{
+        while (guests>=6 || cleaners>0)
+            wait();
+        guests++;
+        System.out.println("Guest enters. \t\tCleaner = "+cleaners+"\tGuest= "+guests);
+    }
+
+    public synchronized void guestExit(){
+        guests--;
+        System.out.println("Guest exits. \t\tCleaner = "+cleaners+"\tGuest= "+guests);
+        notify();
+    }
+
+    public synchronized void cleanerEnter() throws InterruptedException{
+        while(cleaners>0 || guests>0)
+            wait();
+        cleaners++;
+        System.out.println("Cleaner enters. \t\tCleaner = "+cleaners+"\tGuest= "+guests);
+    }
+
+    public synchronized void cleanerExit(){
+        cleaners--;
+        System.out.println("Cleaner exits. \t\tCleaner = "+cleaners+"\tGuest= "+guests);
+        notify();
+    }
 }
